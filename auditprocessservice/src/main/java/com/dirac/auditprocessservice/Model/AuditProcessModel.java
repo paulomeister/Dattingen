3530a9a812eb3dc97db078b732c63459c76b90a6
Sergio Herrera <sergioandresherreravallejo@gmail.com>
2025-05-14 22:40:54 -0500

    New AuditProcess odel

--- auditprocessservice/src/main/java/com/dirac/auditprocessservice/Model/AuditProcessModel.java
+++ auditprocessservice/src/main/java/com/dirac/auditprocessservice/Model/AuditProcessModel.java
@@ -1,8 +1,10 @@
 package com.dirac.auditprocessservice.Model;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -14,18 +16,29 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "auditprocess")
+@Document(collection = "AuditsProcesses")
 public class AuditProcessModel {
-  @JsonIgnore
+
+  @Id
   private String _id;
-  private String bussinessId;
-  private String rulesetId;
-  private String status; // TODO: define status ENUM
-  private List<Criterion> assesment; //TODO: understand what this is
-  private List<Inspector> assignedInt;
-  private List<Inspector> assignedExt;
-  private Date processBegins;
-  private Date processEnds;
+
+  private String bussinessId; //
+  private String rulesetId; //
+  private ProcessStatus status; // Evaluated or not
+  private List<Inspector> assignedIntAuditors;
+  private List<Inspector> assignedExtAuditors;
+
+  private List<Assesment> assesment;
+
+  private Date startDate;
+  private Date endDate;
+
+
+
+
+
+
+
 
   @Data
   @AllArgsConstructor
@@ -38,22 +51,88 @@ public class AuditProcessModel {
   @Data
   @AllArgsConstructor
   @NoArgsConstructor
-  public class Criterion {
-    public String _id;
+  public class Assesment {
+    public String controlId; // SE manda a un endpoint getControl(controlId, rulesetId) para obtener el
+                             // control
+    public AssesmentStatus status;
     public Date assesedIn;
-    public List<Inspector> internals;
-    public List<Inspector> externals;
-    private String comment;
-    private boolean satisfaction; // TODO: understand why
-    private List<Evidence> proof;
-    private String stage;
+    public Inspector internalAuditor;
+    public Inspector externalAuditor;
+    public String comment;
   }
 
   @Data
-  @AllArgsConstructor
-  @NoArgsConstructor  
   public class Evidence {
     private String description;
     private String url;
+    private Date addedDate; // Fecha en que se añadió la evidencia
+
   }
+
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Control {
+    private String controlId;
+    private String title;
+    private String description;
+    private String suitability;
+    private PHVAPhase cycleStage;
+    private ComplianceLevel compulsoriness;
+  }
+
+  public enum ProcessStatus {
+    NOT_EVALUATED,
+    EVALUATED,
+    CANCELED
+  }
+
+  public enum AssesmentStatus {
+    NOT_EVALUATED,
+    COMPLIANT,
+    NON_COMPLIANT,
+    NOT_DONE // Es un estado para cuando el auditor interno diga que no tiene ese criterio
+             // implmentado
+  }
+
+  public enum PHVAPhase {
+    PLAN,
+    DO,
+    CHECK,
+    ACT
+  }
+
+  public enum ComplianceLevel {
+    SHALL("deberá"),
+    MUST("debe"),
+    SHOULD("podrá"),
+    MAY("puede");
+
+    private final String spanishTerm;
+
+    ComplianceLevel(String spanishTerm) {
+      this.spanishTerm = spanishTerm;
+    }
+
+    public String getSpanishTerm() {
+      return spanishTerm;
+    }
+
+    public static List<String> getAllInEnglish() {
+      List<String> englishTerms = new ArrayList<>();
+      for (ComplianceLevel level : ComplianceLevel.values()) {
+        englishTerms.add(level.name()); // `name()` retorna el valor en inglés
+      }
+      return englishTerms;
+    }
+
+    public static List<String> getAllInSpanish() {
+      List<String> spanishTerms = new ArrayList<>();
+      for (ComplianceLevel level : ComplianceLevel.values()) {
+        spanishTerms.add(level.getSpanishTerm()); // Usar `getSpanishTerm()` para obtener el valor en español
+      }
+      return spanishTerms;
+    }
+  }
+
 }
