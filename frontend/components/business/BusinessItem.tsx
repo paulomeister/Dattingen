c7c868d5d660ffa89d680370826062bc33cbde8e
Sergio Herrera <sergioandresherreravallejo@gmail.com>
2025-05-15 23:38:00 -0500

    a

--- frontend/components/business/BusinessItem.tsx
+++ frontend/components/business/BusinessItem.tsx
@@ -24,8 +24,8 @@ export default function BusinessItem({ business }: BusinessItemProps) {
                         <Building2 className="h-6 w-6 text-primary-color" />
                     </div>
                     <div className="flex-1">
-                        <h3 className="text-xl font-medium text-gray-900">{business.name}</h3>
-                        <p className="text-sm text-gray-500 mt-1 line-clamp-2">{business.activity}</p>
+                        <h3 className="text-xl font-medium text-gray-900">{business?.name}</h3>
+                        <p className="text-sm text-gray-500 mt-1 line-clamp-2">{business?.activity.slice(0, 30) + "..."}</p>
                     </div>
                 </div>
 
@@ -33,14 +33,14 @@ export default function BusinessItem({ business }: BusinessItemProps) {
                     <div className="flex items-center">
                         <Users className="h-5 w-5 text-secondary-color mr-2" />
                         <span className="text-sm font-medium">
-                            {business.associates.length} {t("business.associates.title")}
+                            {business?.associates?.length} {t("business.associates.title")}
                         </span>
                     </div>
 
                     <div className="flex items-center">
                         <FileText className="h-5 w-5 text-secondary-color mr-2" />
                         <span className="text-sm font-medium">
-                            {business.audits.length} {t("business.audits")}
+                            {business?.audits?.length} {t("business.audits")}
                         </span>
                     </div>
                 </div>
