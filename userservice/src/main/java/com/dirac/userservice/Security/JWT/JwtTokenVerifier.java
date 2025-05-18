d242c5178c40c7af870cde70d80cc1c31633070b
Jean Paul Delgado Jurado <142752729+paulomeister@users.noreply.github.com>
2025-05-17 20:08:53 -0500

    Update JwtTokenVerifier.java

--- userservice/src/main/java/com/dirac/userservice/Security/JWT/JwtTokenVerifier.java
+++ userservice/src/main/java/com/dirac/userservice/Security/JWT/JwtTokenVerifier.java
@@ -76,12 +76,11 @@ public class JwtTokenVerifier extends OncePerRequestFilter {
         }
         catch(JwtException e) {
 
-            throw new IllegalStateException(String.format("Current token \"%s\" is not trustworthy", token, e.getMessage()));
+            throw new IllegalStateException(String.format("Current token \"%s\" is not trustworthy", token));
 
         }
 
         filterChain.doFilter(request, response);
-
 
     }
 }
