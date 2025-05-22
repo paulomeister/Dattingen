ba158a874f6e47ce800c6bdd9e63a684a3a6fea9
Jean Paul Delgado <jean.delgado@uao.edu.co>
2025-05-21 20:00:23 -0500

    update

--- frontend/env/environment.dev.ts
+++ frontend/env/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API_URL: "http://apigateway:8090",
+  API_URL: "http://172.174.214.41:8090",
   ACMEBUSINESS: "680b0a0db3f93a7e7c7c2c0c", // Replace with your actual business ID
 };
